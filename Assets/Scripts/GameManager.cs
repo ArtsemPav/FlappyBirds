e61eac4bf45c412e80f69243d5b9f6efb4dafa52
ArtsemPav <110222061+ArtsemPav@users.noreply.github.com>
2025-01-24 12:27:15 +0300

    Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,20 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public LoseWindow loseWindow;
+    public GameObject HitSound;
     public static GameManager instance;
     private void Start()
     {
         instance = this;
         loseWindow.gameObject.SetActive(false);
         Time.timeScale = 1;
+        HitSound.SetActive(false);
     }
     public void RestartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
+        HitSound.SetActive(false);
     }
 
     public void Lose()
@@ -22,11 +25,13 @@ public class GameManager : MonoBehaviour
         loseWindow.gameObject.SetActive(true);
         loseWindow.PlayerLose();
         Time.timeScale = 0;
+        HitSound.SetActive(true);
     }
 
     public void LoadScene(int scene)
     {
         SceneManager.LoadScene(scene);
         Time.timeScale = 1;
+        HitSound.SetActive(false);
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public float timeToSpawn, minYPosition, MaxYPosition;
+    public float maxtimeToSpawn = 0.8f;
     public GameObject spawnPrefab;
     private float timer;
-
+    private int counter = 0;
+    public int difficultIncreese = 4;
 
     private void Update()
     {
         if (timer <= 0)
         {
+            counter++;
             timer = timeToSpawn;
             GameObject Pipe = Instantiate(spawnPrefab, transform.position, Quaternion.identity);
             float range = Random.Range(minYPosition, MaxYPosition);
             Pipe.transform.position = new Vector3(Pipe.transform.position.x, range, 0);
             Destroy(Pipe, 15);
+            if ((counter > difficultIncreese) && (timeToSpawn > maxtimeToSpawn))
+            {
+                counter = 0;
+                timeToSpawn = timeToSpawn - 0.2f;
+            }
         }
         else
         {
